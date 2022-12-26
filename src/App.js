1e6ec72c9e46cda25e50c4e3c4b9c943d5e20781
Your NameArniss5 <kat.dobr@gmail.com>
2022-12-26 20:20:09 +0000

    connect css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react"
-import Header from "./components/Header"
+import {Header} from "./components/Header"
 import Meme from "./components/Meme"
-/**
- * Challenge: 
- * - Create a Meme component.
- * - Inside the Meme component, render a styled form
- *   with our 2 inputs and the button.
- * - Don't worry about adding any functionality yet
- */
+import './style.css';
+
+
 export default function App() {
     return (
         <div>
